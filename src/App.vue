f27a28b85d83c5154c3c34438a72c226517e1a03
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-03 07:54:52 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/examples/HelloWorld.vue'
+import TheWelcome from './components/examples/TheWelcome.vue'
 </script>
 
 <template>

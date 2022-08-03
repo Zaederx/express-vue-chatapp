8c92e0b64744d8e35b8e48f5dec5ab90d9d51c99
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-03 07:55:46 +0200

    Added components globally to app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './Home.vue'
+// import './assets/main.css'
+import Banner from './components/Banner.vue'
+import HeadScripts from './components/HeadScripts.vue'
+import SiteFooter from './components/SiteFooter.vue'
 
-import './assets/main.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+// .mount('#app')
+
+app
+.component('Banner',Banner)
+.component('HeadScripts', HeadScripts)
+.component('SiteFooter', SiteFooter)
+
+//last thing to be called from app
+.mount('#app')

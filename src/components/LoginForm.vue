5b5c237131a8332397ec2112e269e4b660f4aeae
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-05 14:14:49 +0200

    Create LoginForm.vue

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="form-control">
+        <form class="form">
+            <label for="email" type="text">Email</label>
+            <input class="form-control" type="text" placeholder="email@email.com"/>
+
+            <label for="password">Password</label>
+            <input class="form-control" type="password" placeholder="Something!Awes@me43^"/>
+
+            <button class="btn btn-primary">Login</button>
+            <button class="btn btn-warning">Sign Up</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+
+</style>

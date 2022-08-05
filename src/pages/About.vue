808dca2088eaf8bbeaa4d0de220175a5cd31f4fa
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-05 14:14:59 +0200

    Create About.vue

---
+++ src/pages/About.vue
@@ -0,0 +1,20 @@
+<!-- JS & VUE -->
+<script setup lang="ts">
+import AboutSection from "../components/AboutSection.vue";
+    
+</script>
+
+<!-- HTML -->
+<template>
+   <HeadScripts></HeadScripts>
+   <Banner heading1="Chat" heading2="Home View"></Banner>
+   <HomeMain>
+    <!-- <AboutSection></AboutSection> -->
+   </HomeMain>
+   <SiteFooter></SiteFooter>
+</template>
+
+<!-- CSS -->
+<style scoped>
+
+</style>

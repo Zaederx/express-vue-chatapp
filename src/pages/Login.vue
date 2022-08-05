89f7ca1e49f42a7ebd044d802db8c71751ebfa43
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-05 14:15:03 +0200

    Create Login.vue

---
+++ src/pages/Login.vue
@@ -0,0 +1,18 @@
+<!-- JS & VUE -->
+<script setup lang="ts">
+</script>
+
+<!-- HTML -->
+<template>
+   <HeadScripts></HeadScripts>
+   <Banner heading1="Chat" heading2="Login"></Banner>
+   <HomeMain>
+    <!-- <LoginForm></LoginForm> -->
+   </HomeMain>
+   <SiteFooter></SiteFooter>
+</template>
+
+<!-- CSS -->
+<style scoped>
+
+</style>

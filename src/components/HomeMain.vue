f062b23c75ea85deeea44b0ad5b85f8aeea56f7b
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-05 14:14:45 +0200

    Create HomeMain.vue

---
+++ src/components/HomeMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="main" class="main">    
+        
+    </div>
+</template>
+
+<style scoped>
+.main {
+    height: 1000px;
+    background-image: url(/img/pexels-pixabay-163064.jpg);
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 100% 100%;
+} 
+</style>

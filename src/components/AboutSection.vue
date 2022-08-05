d0d1bf0dd24902dbad09df2a3d69328e9469fe2d
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-05 14:14:38 +0200

    Create AboutSection.vue

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,18 @@
+<!-- JS & VUE -->
+<script setup lang="ts">
+    
+</script>
+
+<!-- HTML -->
+<template>
+   <div>
+    <p>Lorem ipsum dolor sit amet, consectet. Lorem ipsum dolor sit amet, consectet. Lorem ipsum dolor sit amet, consectet. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio libero fugiat fuga! Sed maiores quia tempore sapiente cumque, ipsam, blanditiis quibusdam, praesentium aperiam inventore expedita quod odio mollitia repudiandae adipisci?</p>
+
+    <p>Lorem ipsum dolor sit am. Lorem ipsum dolor sit am, consect. Lorem ipsum dolor sit am, consectet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, corrupti. Dicta molestias libero cumque molestiae eligendi. Possimus libero ab sed hic magni id facere accusamus, tenetur nostrum odit eum fugiat!</p>
+   </div>
+</template>
+
+<!-- CSS -->
+<style scoped>
+
+</style>

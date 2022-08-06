8f7554120b5dc1ec08cbfcc714b0b09f9eae5e89
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-06 02:46:20 +0200

    Create SignUp.vue

---
+++ src/pages/SignUp.vue
@@ -0,0 +1,9 @@
+<template>
+    <HeadScripts></HeadScripts>
+    <Banner heading1="Chat" heading2="Sign Up"></Banner>
+    <HomeMain>
+        <SignUpForm></SignUpForm>
+    </HomeMain>
+    
+    <SiteFooter></SiteFooter>
+</template>

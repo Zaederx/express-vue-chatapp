3df50d452daa41b3f31b3131934cc2286299641f
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-06 09:24:49 +0200

    Worked on forms

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,15 +1,15 @@
 <template>
     <div id="form-container" class="container form-control center">
-            <form id="sign-up-form" class="form">
-                <label for="email">Email</label>
-                <input id="email" type="email" name="email" class="form-control"/>
-                
-                <label for="password">Password</label>
-                <input id="password" type="password" name="password" class="form-control"/>
+        <form id="sign-up-form" class="form">
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" class="form-control"/>
+            
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" class="form-control"/>
 
-                <button id="btn-sign-up" class="btn btn-primary form-control">Login</button>
-                <button id="btn-login" class="btn btn-warning form-control">Sign Up</button>
-            </form>
+            <button id="btn-sign-up" class="btn btn-primary form-control">Login</button>
+            <button id="btn-login" class="btn btn-warning form-control">Sign Up</button>
+        </form>
     </div>
 </template>
 
--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,17 +1,16 @@
 <template>
     <div id="form-container" class="container form-control center">
-            <form id="sign-up-form" class="form">
-                <label for="email">Email</label>
-                <input id="email" type="email" name="email" class="form-control"/>
-                <label for="name">Name</label>
-                <input id="name" type="text" name="name" class="form-control"/>
-                <label for="password">Password</label>
-                <input id="password" type="password" name="password" class="form-control"/>
+        <form id="sign-up-form" class="form">
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" class="form-control"/>
+            <label for="name">Name</label>
+            <input id="name" type="text" name="name" class="form-control"/>
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" class="form-control"/>
 
-                <button id="btn-sign-up" class="btn btn-primary form-control">Sign Up</button>
-                <button id="btn-login" class="btn btn-warning form-control">Login</button>
-            </form>
-        
+            <button id="btn-sign-up" class="btn btn-primary form-control">Sign Up</button>
+            <button id="btn-login" class="btn btn-warning form-control">Login</button>
+        </form>
     </div>
 </template>
 

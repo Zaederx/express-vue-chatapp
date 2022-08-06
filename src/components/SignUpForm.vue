e76f362c36904660b5545b81ca6f42539db514c8
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-06 10:01:57 +0200

    SignUpForm was missing password2

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -6,7 +6,10 @@
             <label for="name">Name</label>
             <input id="name" type="text" name="name" class="form-control"/>
             <label for="password">Password</label>
-            <input id="password" type="password" name="password" class="form-control"/>
+            <input id="password" type="password" name="password1" class="form-control"/>
+
+            <label for="password">Re-enter Password</label>
+            <input id="password" type="password" name="password2" class="form-control"/>
 
             <button id="btn-sign-up" class="btn btn-primary form-control">Sign Up</button>
             <button id="btn-login" class="btn btn-warning form-control">Login</button>

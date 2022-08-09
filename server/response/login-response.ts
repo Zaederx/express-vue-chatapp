45455aeaea928bf3d181048c7e75b9ae39ac82d0
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-09 13:52:43 +0200

    Create login-response.ts

---
+++ server/response/login-response.ts
@@ -0,0 +1,19 @@
+import { LinkResponse } from "./link-response.js";
+
+export class LoginResponse extends LinkResponse
+{ 
+    userId:number
+
+    /**
+     * 
+     * @param res (response) true when there is information on other the variables to return
+     * @param message a message to be displayed on the client side
+     * @param userId the user id of the current/logging in user
+     * @param link (Optional) a link for http requesting data from node server
+     */
+    constructor(res:boolean, message?:string, userId?:number, link?:string, )
+    {
+        super(res, message, link);
+        this.userId = userId;
+    }
+}

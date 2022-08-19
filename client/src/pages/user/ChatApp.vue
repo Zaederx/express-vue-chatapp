7bd97741c0740571bb520d8e48b5452b431662b7
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-19 09:07:29 +0200

    Update ChatApp.vue

--- client/src/pages/user/ChatApp.vue
+++ client/src/pages/user/ChatApp.vue
@@ -1,4 +1,8 @@
 <template>
+    <HeadScripts></HeadScripts>
+    <Banner heading1="Chat" heading2="ChatApp"></Banner>
+    <HomeMain></HomeMain>
+    <SiteFooter></SiteFooter>
 </template>
 
 <style scoped>

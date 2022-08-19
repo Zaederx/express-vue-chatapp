a8a0e84f9db088cb22ea95640ef11518b4d3982b
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-19 09:07:34 +0200

    Create UserHome.vue

---
+++ client/src/pages/user/UserHome.vue
@@ -0,0 +1,9 @@
+<template>
+    <HeadScripts></HeadScripts>
+    <UserBanner heading1="Chat" heading2="UserHome"></UserBanner>
+    <HomeMain></HomeMain>
+    <SiteFooter></SiteFooter>
+</template>
+
+<style scoped>
+</style>

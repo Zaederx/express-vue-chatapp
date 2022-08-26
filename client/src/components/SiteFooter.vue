8b106016d5f6ca0c038ec0f42cab19738d590205
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-26 18:51:46 +0200

    Update SiteFooter.vue
    Just making it look a bit nicer

--- client/src/components/SiteFooter.vue
+++ client/src/components/SiteFooter.vue
@@ -1,12 +1,6 @@
 <template>
  <div id="footer" class="footer">
     <div class="section-links">
-        <div class="blurb">
-        <p>This is an exmaples site with and example footer. :)
-            Now it's time for some Lorem...
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente cumque aperiam facere accusantium molestiae asperiores. Velit esse explicabo, nostrum, quae delectus illum ipsa eos architecto, voluptatem reiciendis qui soluta officia.
-        </p>
-    </div>
         <div class="grid-item">
             <p>Home</p>
             <p>About</p>
@@ -34,7 +28,9 @@
         <i class="fa fa-lg fa-google-plus"></i>
         <i class="fa fa-lg fa-instagram"></i>
     </div>
-    <div class="copyright">example copyright ChatApp Company</div>
+    <div class="copyright">
+       <div> copyright ChatApp Company</div> 
+    </div>
  </div>
 </template>
 
@@ -45,14 +41,14 @@
     color: white;
     display: grid;
     grid-template-columns: auto;
-    grid-template-rows: [section-links] 4fr [social-media] 4fr [copyright] 2fr;
+    grid-template-rows: [section-links] 3fr [social-media] 4fr [copyright] 3fr;
     /* section 1-links,2-social,3-copyright notice */
 }
 
 .section-links {
     grid-row: 1;
     display: grid;
-    grid-template-columns: 20% repeat(4,18%);
+    grid-template-columns: repeat(4,18%);
     grid-template-rows: auto;
     justify-content: center;
 }
@@ -79,6 +75,7 @@
     grid-template-rows: [single-row] auto;
 }
 .social-media > i {
+    margin-top:25px;
     margin-left: 100px;
     font-size: 60px;
     cursor: pointer;
@@ -89,5 +86,9 @@
     color:white;
     display: grid;
     justify-content: center;
+}
+
+.copyright > div {
+    margin-top: 25px;
 }
 </style>

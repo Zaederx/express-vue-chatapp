fdb2fdd9aca0c7fb89ece728237f7c7e2a066f30
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-27 17:47:56 +0200

    Chat.ts added id

--- server/ts/db/classes/Chat.ts
+++ server/ts/db/classes/Chat.ts
@@ -1,10 +1,12 @@
 import { Message } from './Message.js'
 export class Chat 
 {
+    id:string;
     messages:Message[]
 
-    constructor(messages:Message[])
+    constructor(id:string, messages:Message[])
     {
+        this.id = id
         this.messages = messages
     }
 }

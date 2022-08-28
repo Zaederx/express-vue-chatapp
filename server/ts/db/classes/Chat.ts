4dac441122e5d543d414353e0bfd97b334cb9954
ZIshmael <zacharyk.ishmael@gmail.com>
2022-08-28 17:11:48 +0200

    Chat.ts - update on messages defaulting to an  empty array

--- server/ts/db/classes/Chat.ts
+++ server/ts/db/classes/Chat.ts
@@ -4,7 +4,7 @@ export class Chat
     id:string;
     messages:Message[]
 
-    constructor(id:string, messages:Message[])
+    constructor(id:string, messages:Message[]= [])
     {
         this.id = id
         this.messages = messages

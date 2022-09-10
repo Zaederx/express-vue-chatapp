fffe91ee0d0da826c7c80bd46af9b2260f7038c9
ZIshmael <zacharyk.ishmael@gmail.com>
2022-09-10 17:56:51 +0200

    fade effects on front page

--- client/src/components/HomeMain.vue
+++ client/src/components/HomeMain.vue
@@ -1,10 +1,11 @@
 <template>
+
     <div id="main" class="main">    
         <slot></slot>
     </div>
 </template>
 
-<style scoped>
+<style>
 .main {
     height: 1000px;
     background-image: url(/img/pexels-pixabay-163064.jpg);
@@ -13,5 +14,40 @@
     background-size: 100% 100%;
     display:grid;
     justify-content: center;
+
+    /* Fade in */
+    /* background-color:black; */
+    /* position: absolute; */
+    /* z-index: 1; */
+    /* top: 0; */
+    /* left: 0; */
+    /* width: 100%; */
+    /* min-height: 100%; */
+    animation: fadeInAnimation ease 3s;
+	animation-iteration-count: 1;
+	animation-fill-mode: forwards;
+    
 } 
+
+
+.background {
+    
+}
+
+
+@keyframes fadeInAnimation 
+{
+	0% {
+        /* background: black; */
+		opacity: 0;
+	}
+    /* 50% {
+        background: rgb(27, 27, 27);
+	} */
+	100% {
+        /* background: rgb(69, 69, 69); */
+		opacity: 1;
+	}
+}
+
 </style>

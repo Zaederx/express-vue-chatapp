eaf2c59c517b3c4345c02d57d45ae4aed3e6a4a7
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-15 12:45:59 +0200

    Shorten user list + delete db file if exists

--- server/ts/db/db-setup-test.ts
+++ server/ts/db/db-setup-test.ts
@@ -5,6 +5,14 @@ import { Data } from './classes/Data.js';
 import { User } from './classes/User.js';
 import bcryptjs from 'bcryptjs'
 import { Chat } from './classes/Chat.js';
+import { unlink } from 'node:fs';
+
+//delete existing json file if any
+unlink('db-test.json', (err) => {
+  if (err) throw err;
+  console.log('db-test.json was deleted');
+});
+
 //use json file for storage
 const file = path.join('db-test.json')
 
@@ -17,20 +25,37 @@ setup()
 async function setup() {
 //Read data from Json file, this will set the db.content
 await db.read();
-//clear db
+//clear db file
+db.data = null
 db.data = {users: []}
+
 //if file.json does not exist db.data will be null
 //Set data to default
 // db.data ||= {users: []}
 const password = 'password'
 const passwordHash = bcryptjs.hashSync(password, 10)
 
+var u0:User = new User({email:'email0@email.com',
+                        name:'name 0' ,
+                        username:'username0',
+                        password:passwordHash
+                    });
+u0.sessionId = '555b975a-39e2-4b9f-b62a-7da7d5b8360a'
+
 var u1:User = new User({email:'email1@email.com',
                         name:'name 1' ,
                         username:'username1',
                         password:passwordHash
-                    });
-u1.sessionId = '555b975a-39e2-4b9f-b62a-7da7d5b8360a'
+                        });
+
+//create test chat to use with users 1 and 2
+var name = 'Test Chat'
+var chatId = '9d4b8a70-5ddc-4a90-aaac-3acd10596187'
+var messages = []
+var subscriberIds = ['0','1']
+var c1 = new Chat(chatId,name,[],subscriberIds)
+u0.chats.push(c1)
+u1.chats.push(c1)
 
 var u2:User = new User({email:'email2@email.com',
                         name:'name 2' ,
@@ -38,14 +63,7 @@ var u2:User = new User({email:'email2@email.com',
                         password:passwordHash
                         });
 
-//create test chat to use with users 1 and 2
-var name = 'test Chat'
-var chatId = '9d4b8a70-5ddc-4a90-aaac-3acd10596187'
-var messages = []
-var subscriberIds = ['0','1']
-var c1 = new Chat(chatId,name,[],subscriberIds)
-u1.chats.push(c1)
-u2.chats.push(c1)
+
 
 var u3:User = new User({email:'email3@email.com',
                         name:'name 3' ,
@@ -54,78 +72,15 @@ var u3:User = new User({email:'email3@email.com',
                         });
 
 
-
-var u4:User = new User({email:'email4@email.com',
-                        name:'name 4' ,
-                        username:'username4',
-                        password:passwordHash
-                        });
-
-var u5:User = new User({email:'email5@email.com',
-                        name:'name 5' ,
-                        username:'username5',
-                        password:passwordHash
-                        });
-
-
-var u6:User = new User({email:'email6@email.com',
-                        name:'name 6' ,
-                        username:'username6',
-                        password:passwordHash
-                        });
-
-var u7:User = new User({email:'email7@email.com',
-                        name:'name 7' ,
-                        username:'username7',
-                        password:passwordHash
-                        });
-
-var u8:User = new User({email:'email8@email.com',
-                        name:'name 8' ,
-                        username:'username8',
-                        password:passwordHash
-                        });
-
-var u9:User = new User({email:'email9@email.com',
-                        name:'name 9' ,
-                        username:'username9',
-                        password:passwordHash
-                        });
-
-var u10:User = new User({email:'email10@email.com',
-                        name:'name 10' ,
-                        username:'username10',
-                        password:passwordHash
-                        });
-
-var u11:User = new User({email:'email11@email.com',
-                        name:'name 11' ,
-                        username:'username11',
-                        password:passwordHash
-                        });
-
-var u12:User = new User({email:'email12@email.com',
-                        name:'name 12' ,
-                        username:'username12',
-                        password:passwordHash
-                        });
-
 //add friends to u1
-u1.friendIds.push(u2.id,u3.id,u4.id,u5.id,u6.id,u7.id)
+u0.friendIds.push(u1.id,u2.id,u3.id)
 
 //add all users and their information to database
+db.data.users.push(u0)
 db.data.users.push(u1)
 db.data.users.push(u2)
 db.data.users.push(u3)
-db.data.users.push(u4)
-db.data.users.push(u5)
-db.data.users.push(u6)
-db.data.users.push(u7)
-db.data.users.push(u8)
-db.data.users.push(u9)
-db.data.users.push(u10)
-db.data.users.push(u11)
-db.data.users.push(u12)
+
 //write this data to json file
 await db.write()
 
@@ -134,8 +89,8 @@ await db.write()
  * needs to be in this order so that db is filled before being queried for users
  */
 await db.read()
-//add u1 id to all friends
-u1.friendIds.forEach(fId => db.data?.users.find(u => u.id == fId)?.friendIds.push(u1.id))
+//add u0 id to all friends
+u1.friendIds.forEach(fId => db.data?.users.find(u => u.id == fId)?.friendIds.push(u0.id))
 await db.write()
 
 }

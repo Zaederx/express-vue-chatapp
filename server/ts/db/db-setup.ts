4f9ff309b09533da81d2228606b48a7a4f66e28d
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2025-04-15 14:06:53 +0200

    Update db-setup.ts

--- server/ts/db/db-setup.ts
+++ server/ts/db/db-setup.ts
@@ -4,84 +4,72 @@ import { Low, JSONFile } from 'lowdb';
 import { Data } from "./classes/Data.js";
 import { User } from "./classes/User.js";
 import bcryptjs from 'bcryptjs'
-import { unlink } from 'node:fs';
+import { unlink, existsSync } from 'node:fs';
+
+const filepath = path.join('db.json')
+console.log('db.json filepath:',filepath)
 
 //delete existing json file if any
-unlink('db.json', (err) => {
-  if (err) throw err;
-  console.log('db.json was deleted');
-});
-//use json file for storage
-const file = path.join('db.json')
+if (existsSync(filepath)) {
+  unlink(filepath, (err) => {
+    if (err) throw err;
+    console.log('db.json was deleted');
+  });
+}
 
-const adapter = new JSONFile<Data>(file);
+//use json file for storage
+const adapter = new JSONFile<Data>(filepath);
 var db = new Low(adapter)
 
-
-
-
-
-setup()
+//call setup
+await setup()
 async function setup() {
 //Read data from Json file, this will set the db.content
 await db.read();
-//if file.json does not exist db.data will be null
 //Set data to default
 db.data ||= {users: []}
 const password = 'password'
 const passwordHash = bcryptjs.hashSync(password, 10)
 
-var u1:User = new User({email:'email0@email.com',
-                        name:'name 1' ,
+var u0:User = new User({email:'email0@email.com',
+                        name:'name 0' ,
                         username:'username0',
                         password:passwordHash
                     });
 
-var u2:User = new User({email:'email1@email.com',
-                        name:'name 2' ,
+var u1:User = new User({email:'email1@email.com',
+                        name:'name 1' ,
                         username:'username1',
                         password:passwordHash
                         });
 
-var u3:User = new User({email:'email2@email.com',
+var u2:User = new User({email:'email2@email.com',
+                        name:'name 2' ,
+                        username:'username2',
+                        password:passwordHash
+                        });
+
+var u3:User = new User({email:'email3@email.com',
                         name:'name 3' ,
                         username:'username3',
                         password:passwordHash
                         });
 
-var u4:User = new User({email:'email1@email.com',
+var u4:User = new User({email:'email4@email.com',
                         name:'name 4' ,
                         username:'username4',
                         password:passwordHash
                         });
 
-var u5:User = new User({email:'email2@email.com',
-                        name:'name 5' ,
-                        username:'username5',
-                        password:passwordHash
-                        });
 
-
-var u6:User = new User({email:'email1@email.com',
-                        name:'name 6' ,
-                        username:'username6',
-                        password:passwordHash
-                        });
-
-var u7:User = new User({email:'email2@email.com',
-                        name:'name 7' ,
-                        username:'username7',
-                        password:passwordHash
-                        });
 
 //add friends to u1
-u1.friendIds.push(u2.id,u3.id,u4.id,u5.id,u6.id,u7.id)
-u2.friendIds.push(u1.id,u3.id,u4.id,u5.id,u6.id,u7.id)
-u3.friendIds.push(u1.id,u2.id)
-u4.friendIds.push(u1.id,u2.id)
-u5.friendIds.push(u1.id,u2.id)
-u6.friendIds.push(u1.id,u2.id)
-u7.friendIds.push(u1.id,u2.id)
+u0.friendIds.push(u1.id,u2.id,u3.id,u4.id)
+u1.friendIds.push(u0.id,u2.id,u3.id,u4.id)
+u2.friendIds.push(u0.id,u1.id,u3.id,u4.id)
+u3.friendIds.push(u0.id,u1.id,u2.id,u4.id)
+u4.friendIds.push(u0.id,u1.id,u2.id,u3.id)
+
 
 
 
@@ -90,9 +78,6 @@ db.data.users.push(u1)
 db.data.users.push(u2)
 db.data.users.push(u3)
 db.data.users.push(u4)
-db.data.users.push(u5)
-db.data.users.push(u6)
-db.data.users.push(u7)
 
 //write this data to json file
 await db.write()
